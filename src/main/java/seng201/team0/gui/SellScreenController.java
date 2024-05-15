d960abe62f4f35e10279503c71a444c04ae3b634
fbl24 <fbl24@uclive.ac.nz>
2024-05-15 13:52:40 +1200

    Added fx:id's for the sell_screen.fxml

--- src/main/java/seng201/team0/gui/SellScreenController.java
+++ src/main/java/seng201/team0/gui/SellScreenController.java
@@ -1,4 +1,13 @@
 package seng201.team0.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class SellScreenController {
+
+    @FXML Button sellTower1Button, sellTower2Button, sellTower3Button, sellTower4Button, sellTower5Button, sellTower6Button, sellTower7Button, sellTower8Button;
+    @FXML Button sellItem1Button, sellItem2Button, sellItem3Button, sellItem4Button;
+    @FXML Label sellPriceSelectedObjectButton;
+    @FXML Button sellSelectedObjectButton, goToInventoryButton;
+    @FXML Label selectedTowerTypeLabel, selectedTowerCollectionAmountLabel, selectedTowerCollectionSpeedLabel;
 }
