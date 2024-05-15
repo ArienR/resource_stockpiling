57ba3a31277d6a90534c6b953fdc639f6ea1a48e
fbl24 <fbl24@uclive.ac.nz>
2024-05-15 13:37:54 +1200

    Added fx:id's for the inventory_screen.fxml

--- src/main/java/seng201/team0/gui/InventoryScreenController.java
+++ src/main/java/seng201/team0/gui/InventoryScreenController.java
@@ -1,15 +1,58 @@
 package seng201.team0.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import seng201.team0.GameManager;
 
 public class InventoryScreenController {
 
+    //attributes
+    @FXML
+    private Label inventoryMessageLabel;
+
+    @FXML
+    private Button inventoryTower1Button;
+    @FXML
+    private Button inventoryTower2Button;
+    @FXML
+    private Button inventoryTower3Button;
+    @FXML
+    private Button inventoryTower4Button;
+    @FXML
+    private Button inventoryTower5Button;
+    @FXML
+    private Button inventoryTower6Button;
+
+    @FXML
+    private Button inventoryTower7Button;
+    @FXML
+    private Button inventoryTower8Button;
+    @FXML
+    private Label inventorySelectedTowerType;
+    @FXML
+    private Label inventorySelectedTowerCollectionAmount;
+    @FXML
+    private Label inventorySelectedTowerCollectionSpeed;
+    @FXML
+    private Button inventoryItem1Button, inventoryItem2Button, inventoryItem3Button, inventoryItem4Button;
+    @FXML
+    private Button inventorySelectedTower1, inventorySelectedTower2, inventorySelectedTower3, inventorySelectedTower4, inventorySelectedTower5;
+    @FXML
+    private Button inventorySelectedItem1, inventorySelectedItem2;
+    @FXML
+    private Label inventorySelectedItemNameLabel, inventorySelectedItemType, inventorySelectedItemStat;
+    @FXML
+    private Button inventoryStartButton, inventoryShopButton, inventorySellButton;
+
     private GameManager gameManager;
 
+
+    // constructor
     public InventoryScreenController(GameManager tempGameManager) {
         this.gameManager = tempGameManager;
     }
 
+    // methods
     public void initialize() {
 
     }
