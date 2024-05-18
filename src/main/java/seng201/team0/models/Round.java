98de4cc039103b7d90894a81c11d7da3a837bae6
fbl24 <fbl24@uclive.ac.nz>
2024-05-18 21:08:01 +1200

    created game state service for handling when a round is over and determining if a game is won / lost. Also added functionality of this serivce to buttons on game screen controller.

--- src/main/java/seng201/team0/models/Round.java
+++ src/main/java/seng201/team0/models/Round.java
@@ -9,14 +9,25 @@ public class Round {
 
 
 //  Constructor for first round
-    public Round(int tempProduceCount, int tempCartSpeedPercentage) {
+    public Round(int tempProduceCount, int tempMeatCount, int tempDairyCount, int tempCartSpeedPercentage) {
         this.produceCount = tempProduceCount;
+        this.meatCount = tempMeatCount;
+        this.dairyCount = tempDairyCount;
         this.cartSpeedPercentage = tempCartSpeedPercentage;
     }
 
     public int getProduceCount() {
+        System.out.println("P" +produceCount);
         return produceCount;
     }
+
+    public int getMeatCount(){
+        System.out.println("M" + meatCount);
+        return meatCount;}
+
+    public int getDairyCount(){
+        System.out.println("D" + dairyCount);
+        return dairyCount;}
 
     public int getCartSpeedPercentage() {
         return cartSpeedPercentage;
