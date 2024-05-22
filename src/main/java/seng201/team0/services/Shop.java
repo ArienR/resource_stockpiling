2da6281a81202e853cbbab4d033a6c414015d38d
ara269 <ara269@uclive.ac.nz>
2024-05-22 16:59:07 +1200

    Completed Javadoc for Shop class.

--- src/main/java/seng201/team0/services/Shop.java
+++ src/main/java/seng201/team0/services/Shop.java
@@ -12,12 +12,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The Shop class represents the in-game shop from which the player will buy
+ * towers and items to prepare for the upcoming round they have selected. The
+ * generates towers and items based on the current round number.
+ */
 public class Shop {
 
+    /**
+     * Reference to the GameManager singleton managing the game state.
+     */
     GameManager gameManager;
+
+    /**
+     * List of towers to be displayed in the shop for purchase.
+     */
     private final List<Tower> towers;
+
+    /**
+     * List of items to be displayed in the shop for purchase.
+     */
     private final List<Item> items;
 
+    /**
+     * Constructs a new Shop which is associated with the GamaManager
+     * depending on the current round number.
+     *
+     * @param gameManager the GameManager managing the game state
+     */
     public Shop(GameManager gameManager) {
         this.gameManager = gameManager;
         towers = new ArrayList<>();
@@ -26,7 +48,11 @@ public class Shop {
         generateRandomItems();
     }
 
-    // This still needs to be fixed so that we can assure the player gets at least enough of the particular tower to prepare for the round
+    /**
+     * Generates a list of random towers available for purchase in the shop
+     * depending on the current round number, and assures there is sufficient
+     * towers for the player to prepare for the upcoming round.
+     */
     private void generateRandomTowers() {
         Random rand = new Random();
         int currentRoundNumber = gameManager.getCurrentRoundNumber();
@@ -62,6 +88,12 @@ public class Shop {
         }
     }
 
+    /**
+     * Generates a new ProduceTower with random attributes.
+     *
+     * @param rand the Random object for randomisation
+     * @return a new ProduceTower with random attributes
+     */
     private ProduceTower generateProduceTower(Random rand) {
         int towerSpeed = rand.nextInt(1, 4);
         int towerFillAmount = rand.nextInt(80, 120);
@@ -70,6 +102,12 @@ public class Shop {
         return new ProduceTower(towerSpeed, towerFillAmount, buyPrice, gameBonus);
     }
 
+    /**
+     * Generates a new MeatTower with random attributes.
+     *
+     * @param rand the Random object for randomisation
+     * @return a new MeatTower with random attributes
+     */
     private MeatTower generateMeatTower(Random rand) {
         int towerSpeed = rand.nextInt(1, 4);
         int towerFillAmount = rand.nextInt(80, 120);
@@ -78,6 +116,12 @@ public class Shop {
         return new MeatTower(towerSpeed, towerFillAmount, buyPrice, gameBonus);
     }
 
+    /**
+     * Generates a new DairyTower with random attributes.
+     *
+     * @param rand the Random object for randomisation
+     * @return a new DairyTower with random attributes
+     */
     private DairyTower generateDairyTower(Random rand) {
         int towerSpeed = rand.nextInt(1, 4);
         int towerFillAmount = rand.nextInt(80, 120);
@@ -86,6 +130,10 @@ public class Shop {
         return new DairyTower(towerSpeed, towerFillAmount, buyPrice, gameBonus);
     }
 
+    /**
+     * Generates a list of random items that are purchasable from the shop, and assures
+     * there are sufficient items for the upcoming round.
+     */
     private void generateRandomItems() {
         Random rand = new Random();
         for (int i = 0; i < 2; i++) {
@@ -98,10 +146,18 @@ public class Shop {
         }
     }
 
+    /**
+     * Gets the randomly generated towers.
+     * @return randomly generated towers
+     */
     public List<Tower> getTowers() {
         return towers;
     }
 
+    /**
+     * Gets the randomly generated items.
+     * @return randomly generated items
+     */
     public List<Item> getItems() {
         return items;
     }
