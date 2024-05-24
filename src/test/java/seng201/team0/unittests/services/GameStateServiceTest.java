0082b4aa1ec20320ad646108faecb760bf4cc333
fbl24 <fbl24@uclive.ac.nz>
2024-05-24 21:04:33 +1200

    update money earnings per round
    Java testing game state service

--- src/test/java/seng201/team0/unittests/services/GameStateServiceTest.java
+++ src/test/java/seng201/team0/unittests/services/GameStateServiceTest.java
@@ -1,23 +1,37 @@
-package seng201.team0.unittests.services;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import seng201.team0.GameManager;
-import seng201.team0.Player;
-
-public class GameStateServiceTest {
-
-    private Player player;
-    private GameManager gameManager;
-
-    @BeforeEach
-    public void setupTest() {
-        player = new Player("John Doe");
-        gameManager = new GameManager(player);
-    }
-
-    @Test
-    void checkIsEndOfGame() {
-
-    }
-}
+//package seng201.team0.unittests.services;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import seng201.team0.GameManager;
+//import seng201.team0.Player;
+//import seng201.team0.services.GameStateService;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//public class GameStateServiceTest {
+//
+//    private Player player;
+//    private GameManager testGameManager;
+//    private GameStateService testGameStateService;
+//
+//
+//    @BeforeEach
+//    public void setupTest() {
+//        player = new Player("John Doe");
+//        testGameManager = new GameManager(player);
+//        testGameStateService = new GameStateService(testGameManager);
+//    }
+//
+//    @Test
+//    void testRoundWonGameContinues() {
+//        testGameManager.setNumberOfRounds(5);
+//        testGameManager.incrementCurrentRoundNumber();
+//        testGameManager.incrementCurrentRoundNumber();
+//        testGameManager.incrementCurrentRoundNumber();
+//        testGameManager.setRoundWon(true);
+//        System.out.println(testGameManager.getCurrentRoundNumber());
+//        testGameStateService.isEndOfGame();
+//        testGameStateService.randomEvents();
+//        testGameStateService.usedTowerRandomEvent(tower);
+//        assertEquals(false, );
+//    }
+//}
